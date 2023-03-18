4aac7cdb7da5b11ccdd308404a3e585fd4a0f9a2
Shraddha <jadhavshradddha5@gmail.com>
2023-03-18 08:26:55 +0530

    Given the State Census CSV File if incorrect Returns a custom Exception

--- src/test/java/CensusAnalyserTestFile.java
+++ src/test/java/CensusAnalyserTestFile.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CensusAnalyserTestFile {
+
+
+    private static final String WRONG_CSV_FILE_PATH = "State.csv";
     private String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\Icon\\IdeaProjects\\Indian_States_Census_Analyser\\src\\main\\resources\\StateCensusData.csv";
 
     //TC 1.1
@@ -13,5 +16,13 @@ public class CensusAnalyserTestFile {
         int count = censusAnalyser.loadIndiaCensusData(INDIAN_CENSUS_CSV_FILE_PATH);
         Assertions.assertEquals(29, count);
 
+    }
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowExceptionSadTest() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
+        CensusAnalyzerCustomException exceptionRule = Assertions.assertThrows(CensusAnalyzerCustomException.class,()->{
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        });
+        Assertions.assertEquals(CENSUS_FILE_PROBLEM, exceptionRule.type);
     }
 }
